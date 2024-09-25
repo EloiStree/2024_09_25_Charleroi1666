1c8516c939bf418aeaca52a9fd99b8adf319c1f0
Eloi Strée <eloistree@gmail.com>
2024-09-25 19:56:05 +0200

    Save: 09/25/2024 7:56 PM

---
+++ Runtime/Charleroi1666SourceMono.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Charleroi1666SourceMono : MonoBehaviour
+{
+    [TextArea(0, 5)]
+    public string m_description = "Charleroi 1666 is a map of the fort. It is square and fixed in time, with a 10-meter base, making it perfect for AR calibration.";
+    public string m_source= "http://darvillealphonse.canalblog.com/archives/2020/12/20/38717766.html";
+
+
+
+}
